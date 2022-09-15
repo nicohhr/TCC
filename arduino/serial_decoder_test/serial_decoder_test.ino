9da558e56919fa80e1eb736783d24590a1b6cbdf
Nicolas Abraham <nicohhr@gmail.com>
2022-09-14 21:27:56 -0300

    Test for decoding of serial package

---
+++ arduino/serial_decoder_test/serial_decoder_test.ino
@@ -0,0 +1,73 @@
+// Incluindo bibliotecas
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define MOTOR_CNT 6
+#define RELE 8
+#define SERVOMIN  125 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  530 // This is the 'maximum' pulse length count (out of 4096)
+
+// Array de bytes com posição das juntas
+uint8_t motorPositions[MOTOR_CNT];
+int positions[DATA_BYTES];
+
+// Instanciando Objeto PWM Servo Driver
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); 
+
+// Funções
+int angle2Bytes(long angle){
+  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+void setup() {
+  
+  // Definindo pinos de entrada/saida
+  pinMode(RELE, OUTPUT);
+
+  // Alimentando servo motores 
+  digitalWrite(RELE, HIGH);
+  delay(300);
+
+  // Iniciando Servoss
+  pwm.begin();
+  pwm.setPWMFreq(50);
+  delay(300);
+
+  // Iniciando comunicação Serial
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // Conferindo se há dados seriais recebidos
+  if (Serial.available()) {
+    
+    // Checando por bytes do header -> "OP"
+    if (Serial.read() == 'O' && Serial.read() == 'P') {
+
+      // Lendo próximos 6 bytes com valores de posição de cada junta (servomotor)
+      Serial.readBytes(motorPositions, MOTOR_CNT);
+
+      // Talvez conferir que cada valor esteja entre 0 e 180
+
+      // Conferindo finalização do pacote -> "ED"
+      if (Serial.read() == 'E' && Serial.read() == 'D') {
+        
+        // Atualizando posição dos motores. 
+        // Valores apenas serão escritos se o apcote for válido
+        for (int m = 0; m < MOTOR_CNT; m++) {
+          pwm.setPWM(m, 0, angle2Bytes(motorPositions[m]));
+        }
+      }
+    }
+  }
+
+  // Retornando valores recebidos
+  for (int i = 0; i < MOTOR_CNT; i++) {
+    Serial.print(" "); 
+    Serial.print(motorPositions[i]); 
+  }
+
+  Serial.println();
+  delay(5);
+}
---
+++ inoSerial_encoder_test.py
@@ -0,0 +1,153 @@
+import math
+import pygame 
+from pygame.constants import*
+
+import serial 
+import time 
+
+def map(x, in_min, in_max, out_min, out_max) -> float:
+    return float((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+# Definindo taxas de velocidade (aceleração)
+xlr8_m1 = 10
+xlr8_m2 = 10
+
+# Posição inicial dos motores/juntas
+m1 = 90
+m2 = 90
+m3 = 90
+m4 = 90
+m5 = 90
+m6 = 90
+
+# Incializando instância de jogo
+pygame.init()
+pygame.joystick.init()
+pygame.event.pump()
+
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+
+# Inicializando instância serial
+serialObj = serial.Serial() 
+
+# Definindo propriedades 
+serialObj.baudrate = 9600
+serialObj.port = 'COM5'
+serialObj.open()
+
+# Teste: Enviando letra A
+print("CTRL + C para finalizar...")
+byte_msg = bytearray([79, 80, int(m1), int(m2), int(m3), int(m4), int(m5), int(m6), 69, 68])
+
+m1_plus = -1
+m1_minus = -1
+
+# Motor a ser movimentado
+motor_selection = 1
+
+
+while True:
+
+    for event in pygame.event.get():
+
+        # Conferindo se há algum botão selecionado
+        if event.type == pygame.JOYHATMOTION:
+
+            x_hat = pygame.joystick.Joystick(0).get_hat(0)[0]
+            
+            # Definindo motor selecionado a partir do pad direcional 
+            if x_hat == 1 and (motor_selection < 6): 
+                motor_selection = motor_selection + 1 
+
+            elif x_hat == -1 and (motor_selection > 1):
+                motor_selection = motor_selection - 1
+
+                
+
+        # Conferindo se houve alteração na posição dos gatilhos
+        if event.type == JOYAXISMOTION:
+
+            # Obtendo valor do Joystick 
+            m1_plus = pygame.joystick.Joystick(0).get_axis(5)
+            m1_minus = pygame.joystick.Joystick(0).get_axis(4)
+    
+
+
+
+    # Definindo razão de aceleração a partir da entrada
+    m1_minus_ratio = map(m1_minus, -1, 1, 0, 1)
+    m1_plus_ratio = map(m1_plus, -1, 1, 0, 1)
+                    
+    # Somando taxa de velocidade pela razão do gatilho
+    match motor_selection:
+        case 1: 
+            m1 = m1 + xlr8_m1*m1_plus_ratio
+            m1 = m1 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m1 > 180:
+                m1 = 180
+            elif m1 < 0: 
+                m1 = 0   
+        case 2:
+            m2 = m2 + xlr8_m1*m1_plus_ratio
+            m2 = m2 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m2 > 180:
+                m2 = 180
+            elif m2 < 0: 
+                m2 = 0   
+        case 3:
+            m3 = m3 + xlr8_m1*m1_plus_ratio
+            m3 = m3 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m3 > 180:
+                m3 = 180
+            elif m3 < 0: 
+                m3 = 0   
+        case 4: 
+            m4 = m4 + xlr8_m1*m1_plus_ratio
+            m4 = m4 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m4 > 180:
+                m4 = 180
+            elif m4 < 0: 
+                m4 = 0   
+        case 5: 
+            m5 = m5 + xlr8_m1*m1_plus_ratio
+            m5 = m5 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m5 > 180:
+                m5 = 180
+            elif m5 < 0: 
+                m5 = 0   
+        case 6: 
+            m6 = m6 + xlr8_m1*m1_plus_ratio
+            m6 = m6 - xlr8_m1*m1_minus_ratio
+
+            # Conferindo que valor não passe de 180 nem seja menor que 0 
+            if m6 > 180:
+                m6 = 180
+            elif m6 < 0: 
+                m6 = 0   
+    
+
+    # Atualizando array de bytes 
+    byte_msg = bytearray([79, 80, int(m1), int(m2), int(m3), int(m4), int(m5), int(m6), 69, 68])
+
+    #print("Trigger Pos: ", trigger_pos)
+    #print("Ratio Plus: ", m1_plus_ratio)
+    #print("Ratio Minus: ", m1_minus_ratio)
+    #print("M1: ", m1)
+
+    serialObj.write(byte_msg)
+    time.sleep(0.02)
+
+    if serialObj.in_waiting: 
+        packet = serialObj.readline()
+        print(packet.decode())
